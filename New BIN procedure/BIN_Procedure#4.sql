e2effa471f170861bb07023a5247ff69bc86a2e1
Lampros Nestoras <lnestoras@dir.eeft.com>
2019-01-21 12:20:50 +0200

    initialCommit_newBINproc

---
+++ New BIN procedure/BIN_Procedure#4.sql
@@ -0,0 +1,37 @@
+--//    1  Delete and then Insert banks into Banks_new
+delete from abc096.banks_new;
+
+insert into zacreporting.abc096.banks_new
+select * from zacreporting.abc096.banks;
+
+
+--//    2  Delete and then insert records of BINs,Ranking, bankdescr to temp file(zacreporting.dbo.banknbins)
+delete from zacreporting.dbo.banknbins;
+
+with trxbins as (
+  select   substring(mask,1,6) binsix  from zacreporting.abc096.IMP_TRANSACT_D_monthly
+  group by substring(mask,1,6)
+  having count(*) > 50
+)
+  insert into zacreporting.dbo.banknbins
+    select (cast(Rank() OVER( ORDER BY bank ) AS int)) + 41 'rownumber' ,bank, bin  from  zacreporting.dbo.binbase a
+    inner join trxbins on binsix = a.bin
+    where a.isocountry <> 'greece' and bank <> ' '
+    group  by bank, bin;
+  --order by a.isocountry     (only for select!!)
+
+
+--//    3   Insert banks from Temp file into  banks_new
+
+insert into zacreporting.abc096.banks_new
+  select bankid, bankdescr  , ' ',  ' ', bankdescr
+  from zacreporting.dbo.banknbins
+  group by bankid, bankdescr;
+
+
+--//     4 Update products file with bankid for existing BINs
+
+  update  a
+  set a.bankid = b.bankid
+  from zacreporting.dbo.products as a
+  inner join zacreporting.dbo.banknbins b on b.binnumber = a.bin;
---
+++ New BIN procedure/BIN_Procedure_Instructions.sql
@@ -0,0 +1,133 @@
+--1. Delete
+        delete from dbo.binbase_forTransfer
+ -- & Insert BINs to temp BIN file, [dbo].[binbase_forTransfer]
+ -- Use below insert for columns
+ insert into dbo.binbase_forTransfer(id, bin, brand, bank, typeb, levelb, isocountry, isoa2, isoa3, isonumber, www, phone) VALUES
+--2. Run sql statement so find which BINs doesn't exist in Products from new file
+
+    select * from [dbo].[binbase_forTransfer] a
+    where not exists (select bin from abc096.Products b where a.bin = b.bin );
+
+--3. Delete table dbo.products_cpyofabc and then Insert sql
+
+    delete from dbo.products_cpyofabc;
+
+  /*  insert into dbo.products_cpyofabc
+    select * from abc096.Products;*/
+
+    INSERT INTO [ZacReporting].[dbo].[products_cpyofabc]
+               ([BIN]
+               ,[BINU]
+               ,[BANKID]
+               ,[ExBankID]
+               ,[Proprietary]
+               ,[Product]
+               ,[Exclude]
+               ,[CARDID]
+               ,[Brand])
+         SELECT [BIN]
+          ,[BINU]
+          ,[BANKID]
+          ,[ExBankID]
+          ,[Proprietary]
+          ,[Product]
+          ,[Exclude]
+          ,[CARDID]
+          ,[Brand]
+      FROM [ZacReporting].[abc096].[Products]
+
+--4. Delete [dbo].[binbase#2] and Insert not exist BINs of new file to BINBASE#2
+  INSERT INTO [ZacReporting].[dbo].[binbase#2]
+       ([id]
+       ,[bin]
+       ,[brand]
+       ,[bank]
+       ,[typeb]
+       ,[levelb]
+       ,[isocountry]
+       ,[isoa2]
+       ,[isoa3]
+       ,[isonumber]
+       ,[www]
+       ,[phone]
+       ,[regioneu])
+       select a.*,' '   from dbo.[binbase_forTransfer] a
+       where not exists (select bin from abc096.Products b where a.bin = b.bin )
+
+--5. Update regioneu
+  update dbo.binbase#2
+    set  regioneu = 'Y'
+    where  isocountry in (
+     'Russia'
+    ,'Germany'
+    ,'Turkey'
+    ,'France'
+    ,'United Kingdom'
+    ,'Italy'
+    ,'Spain'
+    ,'Ukraine'
+    ,'Poland'
+    ,'Romania'
+    ,'Netherlands'
+    ,'Belgium'
+    ,'Greece'
+    ,'Czech Republic'
+    ,'Portugal'
+    ,'Sweden'
+    ,'Hungary'
+    ,'Azerbaijan'
+    ,'Belarus'
+    ,'Austria'
+    ,'Switzerland'
+    ,'Bulgaria'
+    ,'Serbia'
+    ,'Denmark'
+    ,'Finland'
+    ,'Slovakia'
+    ,'Norway'
+    ,'Ireland'
+    ,'Croatia'
+    ,'Bosnia and Herzegovina'
+    ,'Georgia'
+    ,'Moldova'
+    ,'Armenia'
+    ,'Lithuania'
+    ,'Albania'
+    ,'Macedonia'
+    ,'Slovenia'
+    ,'Latvia'
+    ,'Kosovo'
+    ,'Estonia'
+    ,'Cyprus'
+    ,'Montenegro'
+    ,'Luxembourg'
+    ,'Malta'
+    ,'Iceland'
+    ,'Jersey'
+    ,'Isle of Man'
+    ,'Andorra'
+    ,'Guernsey'
+    ,'Faroe Islands'
+    ,'Liechtenstein'
+    ,'Monaco'
+    ,'Gibraltar'
+    ,'San Marino'
+    ,'Aland Islands'
+    ,'Svalbard'
+    ,'Vatican City')
+
+--6.  Insert new BINs to products tables, abc096 & dbo,
+
+INSERT INTO [ZacReporting].[dbo].[products]
+           ([BIN]
+           ,[BINU]
+           ,[BANKID]
+           ,[ExBankID]
+           ,[Proprietary]
+           ,[Product]
+           ,[Exclude]
+           ,[CARDID]
+           ,[Brand])
+select bin, bin, 0, 0, cast(0 as bit) ,substring(brand,1,40)+ ' ' + substring(levelb,1,40), cast(0 as bit), 0, substring(brand,1,20)   from dbo.binbase#2
+
+--7. Run BIN_Procedure#4 so to update banks in new products table
