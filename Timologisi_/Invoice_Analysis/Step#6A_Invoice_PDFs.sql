dba27cc5170e9a4957727edd8163f6a6b8f9b2d3
elrik428 <labroukos80@gmail.com>
2019-02-19 15:34:26 +0200

    Commit_Invoice

--- Timologisi_/Invoice_Analysis/Step#6A_Invoice_PDFs.sql
+++ Timologisi_/Invoice_Analysis/Step#6A_Invoice_PDFs.sql
@@ -20,12 +20,14 @@ SELECT [Group]
       ,[DateF]
       ,[DateT]
   FROM [ZacReporting].[dbo].[ePOSMonthTrx5]
-  --WHERE [Group] = 'ABFR0802'
+  WHERE [Group] = 'ABFR0802'
 
 
 ---   Exceptions       //////      --------
+---  Where [GROUP] = 'ALL'
 
--- ePOSMonthTransactions_180101_180131
+-- ePOSMonthTransactions_180101_180131 / pdf 
+--
 SELECT [Group]
       ,[Descr]
       ,[Terminals]
@@ -40,6 +42,13 @@ SELECT [Group]
       ,[DateT]
   FROM [ZacReporting].[dbo].[ePOSMonthTrx5]
   WHERE [Group] Not In ("VIVA Tickets","VIVA eCommerce","VIVA Travel","VIVA Retail","VIVA Services","VIVA Telecoms","VIVA Non Profit","VIVA Utility Bills","VIVA Mobile","VIVA Taxis","VIVA Partners","VIVA Gaming","VIVA Card Present","VIVA Hotels")
+
+-- ePOSMonthTransactions_180101_180131 / csv
+-- 
+SELECT ePOSMonthTrx5.[Group], ePOSMonthTrx5.Descr, ePOSMonthTrx5.Terminals, ePOSMonthTrx5.MonthRent, ePOSMonthTrx5.TotRent, ePOSMonthTrx5.TrxNo, ePOSMonthTrx5.TrxCost, ePOSMonthTrx5.TotTrxCost, ePOSMonthTrx5.TotCost, ePOSMonthTrx5.Period
+FROM ePOSMonthTrx5
+WHERE ePOSMonthTrx5.[Group] Not In ("VIVA Tickets","VIVA eCommerce","VIVA Travel","VIVA Retail","VIVA Services","VIVA Telecoms","VIVA Non Profit","VIVA Utility Bills","VIVA Mobile","VIVA Taxis","VIVA Partners","VIVA Gaming","VIVA Card Present","VIVA Hotels");
+
 
 -- 
 -- ALPHA TIDs & ALPHA TRXs / pfd,csv
@@ -101,3 +110,259 @@ FROM abc096.MERCHANTS INNER JOIN abc096.TIDs ON abc096.MERCHANTS.TID=TIDs.TID
 WHERE (((TIDs.MonthRent)<>0) And ((abc096.MERCHANTS.MID)='000000001100009'))
 GROUP BY TIDs.TID, TIDs.Shop
 ORDER BY TIDs.Shop;
+
+
+--- KOTSOVOLOSInvoice / pdf
+SELECT 'KOTSOVOLOS' AS [Group], Banks.BANK, abc096.TrxMonthKotsovolos.Date, TIDs.Shop, Sum(abc096.TrxMonthKotsovolos.TrxNo) AS SumOfTrxNo, abc096.TrxMonthKotsovolos.Period
+FROM (abc096.TIDs RIGHT JOIN abc096.TrxMonthKotsovolos ON TIDs.TID = abc096.TrxMonthKotsovolos.TID) INNER JOIN abc096.Banks ON abc096.TrxMonthKotsovolos.DESTCOMID = Banks.DESTCOMID
+GROUP BY Banks.BANK, abc096.TrxMonthKotsovolos.Date, TIDs.Shop, abc096.TrxMonthKotsovolos.Period;
+
+
+--- COSMOTEFR_PPC_Invoice / pdf
+SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='COSMOTE FR' AND Descr='COSMOTE FR-PPC'
+
+
+  --- COSMOTEOwnInvoice / pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='COSMOTE'
+
+
+  ---- GERMANOSFR_PPC_Invoice   /  pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='GERMANOS FR' AND Descr='GERMANOS FR-PPC'
+
+
+  ---- GERMANOSOwnInvoice / pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='GERMANOS' 
+  
+
+  ---- IKEAECOMMERCEInvoice  /  pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='IKEA ECOMMERCE' 
+  
+
+  ---- IKEAInvoice / pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='IKEA' 
+
+
+  ---  INTERSPORTInvoice  / pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='INTERSPORT' 
+
+
+  --- LeroyMerlinInvoice  /  pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='LeroyMerlin' 
+  
+
+  ----  LIDLInvoice  /  pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group]='LIDL' 
+  
+  
+  ---- LIFECARDInvoice  /  pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group] In ('LIFECARD')
+
+
+  ---- NOTOSBONUS /  pdf
+SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group] In ('NOTOS BONUS')
+  
+
+  --- NOTOSInvoice / pdf
+  SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group] In ('NOTOS', 'NOTOSRetail')
+  
+
+
+--- OTEOwnInvoice / pdf
+SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group] In ('OTE')
+  
+
+  --- RaxevskyInvoice / pdf
+SELECT [Group]
+      ,[Descr]
+      ,[Terminals]
+      ,[MonthRent]
+      ,[TotRent]
+      ,[TrxNo]
+      ,[TrxCost]
+      ,[TotTrxCost]
+      ,[TotCost]
+      ,[Period]
+      ,[DateF]
+      ,[DateT]
+  FROM [ZacReporting].[dbo].[ePOSMonthTrx]
+  where [Group] In ('Raxevsky')
+  
+
+  ---  VASILOPOULOSInvoiceDetail / pdf
+SELECT 'VASILOPOULOS Detailed Invoice' AS Descr, TIDs.Shop, Sum(abc096.TrxMonthVasilopoulos.TrxNo) AS SumOfTrxNo, abc096.TrxMonthVasilopoulos.Period, 'VASILOPOULOS' AS [Group]
+FROM AB_Stores RIGHT JOIN ((abc096.TrxMonthVasilopoulos LEFT JOIN abc096.TIDs ON abc096.TrxMonthVasilopoulos.TID = TIDs.TID) INNER JOIN abc096.Banks ON abc096.TrxMonthVasilopoulos.DESTCOMID = Banks.DESTCOMID) ON AB_Stores.Store = TIDs.Shop
+GROUP BY TIDs.Shop, abc096.TrxMonthVasilopoulos.Period
+ORDER BY TIDs.Shop;
+
+
+  
+  
--- Timologisi_/Invoice_Analysis/Step#9_ImportMonthly_Reports.sql
+++ Timologisi_/Invoice_Analysis/Step#9_ImportMonthly_Reports.sql
@@ -1,17 +1,14 @@
 --- Reports from Import monthly.SQL
 
--- Duty Free
--- i.
+-- DutyFree_190101_190131.xls
+
 DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
 
--- ii.
 INSERT INTO [ZacReporting].[abc096].[IMP_TRANSACT_D]
  SELECT * FROM [ZacReporting].[abc096].[IMP_TRANSACT_D_monthly] WHERE MID='000000120002800'
 
--- iii.
 Delete from [abc096].[Transactions]
 
--- iv. 
 Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
                                INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
                                MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
@@ -28,44 +25,222 @@ select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR,
                                [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
 from [abc096].[IMP_TRANSACT_D]
 
--- v.
 Delete from [abc096].[IMP_TRANSACT_D]
 
--- vi.
-SELECT  [Group]
-      ,[DateST]
-      ,[PROCBATCH]
-      ,[Category]
-      ,[PackageePOS]
-      ,[Package]
-      ,[CardNbr]
-      ,[Keybrd]
-      ,[CardType]
-      ,[AmountTRx]
-      ,[Instals]
-      ,[Transaction]
-      ,[Response]
-      ,[REVERSED]
-      ,[TimeST]
-      ,[TID]
-      ,[PROCESSED]
-      ,[Shop]
-      ,[MID]
-      ,[DMID]
-      ,[DTID]
-      ,[ePOSBATCH]
-      ,[Brand]
-      ,[BTCODE]
-      ,[ORGSYSTAN]
-      ,[DTSTAMP_INSERT]
-      ,[BORGSYSTAN]
-      ,[AUTHCODE]
-      ,[CASHIERINFO]
-      ,[TrDMID]
-      ,[TrDTID]
-      ,[TrPackage]
-      ,[PEM_DESC]
-      ,[BONUS_Redemption]
-      ,[DTSTAMP]
-  FROM [ZacReporting].[dbo].[ePOSBatchRep]
-  WHERE [Group] = 'DutyFree'
+SELECT   [Group], Ημερομηνία, Ώρα,   Κατηγορία,   ΠακέτοePOS,   Πακέτο,   Κάρτα,   Brand,   Πληκτρ,   Τύπος,   Ποσό,   Δόσεις,   Συναλλαγή,  Απόκριση,   Αντιλογισμός,   PROCESSED,   Shop,   Mid,   TID,   DMID,   DTID,   ePOSBATCH,   PROCBATCH,   ORGSYSTAN AS SYSTAN,   DTSTAMP_INSERT AS BATCHCLOSEDTSTAMP,   AUTHCODE
+FROM ePOSBatchRep
+WHERE [Group]='DutyFree'
+ORDER BY   Ημερομηνία,   Ώρα;
+
+-------         ***    //////    ****
+
+----FFGROUP_190101_190131.xls
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+INSERT INTO [ZacReporting].[abc096].[IMP_TRANSACT_D]
+SELECT * FROM [ZacReporting].[abc096].[IMP_TRANSACT_D_monthly] WHERE MID='000000120002300'
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH, ePOSBatchRep.ORGSYSTAN AS SYSTAN, ePOSBatchRep.DTSTAMP_INSERT AS BATCHCLOSEDTSTAMP
+FROM ePOSBatchRep
+WHERE (((ePOSBatchRep.[Group])='FFGROUP'))
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+-------         ***    //////    ****
+
+-- Ellinikes_Dianomes.xls  /  PLOIA
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+INSERT INTO [ZacReporting].[abc096].[IMP_TRANSACT_D]
+SELECT * FROM [ZacReporting].[abc096].[IMP_TRANSACT_D_monthly] WHERE MID='000000120003510'
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH, ePOSBatchRep.ORGSYSTAN AS SYSTAN, ePOSBatchRep.DTSTAMP_INSERT AS BATCHCLOSEDTSTAMP
+FROM ePOSBatchRep
+WHERE (((ePOSBatchRep.[Group])='Ellinikes_Dianomes'))
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+
+-------         ***    //////    ****
+
+-- Ellinikes_Dianomes.xls   
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+INSERT INTO [ZacReporting].[abc096].[IMP_TRANSACT_D]
+SELECT * FROM [ZacReporting].[abc096].[IMP_TRANSACT_D_monthly] WHERE MID='000000120003500'
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH, ePOSBatchRep.ORGSYSTAN AS SYSTAN, ePOSBatchRep.DTSTAMP_INSERT AS BATCHCLOSEDTSTAMP
+FROM ePOSBatchRep
+WHERE (((ePOSBatchRep.[Group])='Ellinikes_Dianomes'))
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+
+-------         ***    //////    ****
+
+-- NOTOSMonth.xlsx + NOTOSMonthECOMMERCE.xlsx
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+INSERT INTO [ZacReporting].[abc096].[IMP_TRANSACT_D]
+ SELECT * FROM [ZacReporting].[abc096].[IMP_TRANSACT_D_monthly] WHERE MID like '0000001100002%'
+ or MID = '000000110000300' 
+ or MID = '000000078000000' --add notos ecommerce
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+--- NOTOSMonth.xlsx 
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH
+FROM ePOSBatchRep
+WHERE ePOSBatchRep.[Group]='NOTOSKENTRIKA' OR ePOSBatchRep.[Group]='NOTOSRetail' OR ePOSBatchRep.[Group]='NOTOSCOSMETICS'
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+---- NOTOSMonthECOMMERCE.xlsx
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH
+FROM ePOSBatchRep
+WHERE ePOSBatchRep.[Group]='NOTOSECOMMERCE'
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+
+-------         ***    //////    ****
+
+-- AUTOTECHNICA.xlsx
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+insert into [ZacReporting].[abc096].[IMP_TRANSACT_D]
+select * from abc096.IMP_TRANSACT_D_monthly
+where MID in ('000000120004700','000000120004800','000000120004750')
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH
+FROM ePOSBatchRep
+WHERE ePOSBatchRep.[Group]='AUTOTECHNICA'
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
+
+
+-------         ***    //////    ****
+
+---- INTERSPORTmonthly.xlsx
+DELETE FROM [ZacReporting].[abc096].[IMP_TRANSACT_D]
+
+insert into [ZacReporting].[abc096].[IMP_TRANSACT_D]
+select * from abc096.IMP_TRANSACT_D_monthly
+where MID like '0000001200019%'
+
+Delete from [abc096].[Transactions]
+ 
+Insert into [abc096].[Transactions] (TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN]) 
+select TBL, TCODE, MID, TID, MASK, AMOUNT, CURR, 
+                               INST, GRACE, ORIGINATOR, DESTCOMID, PROCCODE, 
+                               MSGID, RESPKIND, REVERSED, BTBL, BTCODE, PROCBATCH, 
+                               ePOSBATCH, POSDATA, BPOSDATA, DTSTAMP, BDTSTAMP, 
+                               ORGSYSTAN, DTSTAMP_INSERT, ProductID, 
+                               USERDATA,BORGSYSTAN,AUTHCODE,CASHIERINFO,DMID,DTID, 
+                               [CASHBACK],[BONUSRED],[PBGAMOUNT],[RRN] 
+from [abc096].[IMP_TRANSACT_D]
+
+Delete from [abc096].[IMP_TRANSACT_D]
+
+SELECT ePOSBatchRep.[Group], ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα, ePOSBatchRep.Κατηγορία, ePOSBatchRep.ΠακέτοePOS, ePOSBatchRep.Πακέτο, ePOSBatchRep.Κάρτα, ePOSBatchRep.Brand, ePOSBatchRep.Πληκτρ, ePOSBatchRep.Τύπος, ePOSBatchRep.Ποσό, ePOSBatchRep.Δόσεις, ePOSBatchRep.Συναλλαγή, ePOSBatchRep.Απόκριση, ePOSBatchRep.Αντιλογισμός, ePOSBatchRep.PROCESSED, ePOSBatchRep.Shop, ePOSBatchRep.Mid, ePOSBatchRep.TID, ePOSBatchRep.DMID, ePOSBatchRep.DTID, ePOSBatchRep.ePOSBATCH, ePOSBatchRep.PROCBATCH, ePOSBatchRep.BONUS_Redemption
+FROM ePOSBatchRep
+WHERE ePOSBatchRep.[Group] like 'INTERSPORT%'
+ORDER BY ePOSBatchRep.Ημερομηνία, ePOSBatchRep.Ώρα;
