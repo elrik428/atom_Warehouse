097823c90983d769d96f3aff828694930637768d
elrik428 <labroukos80@gmail.com>
2019-04-08 16:34:14 +0300

    Small_FormatChange

--- Reports/Ingenico terminals/MinMaxCount.sql
+++ Reports/Ingenico terminals/MinMaxCount.sql
@@ -1,3 +1,4 @@
-select tid, min(a.dtstamp), max(a.dtstamp), count(*) from (select tid, DTSTAMP from abc096.IMP_TRANSACT_D_2018 a union select tid, DTSTAMP from abc096.IMP_TRANSACT_D_2018_B) a
+select tid, min(a.dtstamp), max(a.dtstamp), count(*)
+from (select tid, DTSTAMP from abc096.IMP_TRANSACT_D_2018 a union select tid, DTSTAMP from abc096.IMP_TRANSACT_D_2018_B) a
 where  exists(select TID from abc096.temp_tid b where a.tid = b.TID )
 group by TID
