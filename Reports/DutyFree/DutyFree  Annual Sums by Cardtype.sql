8209e6c82830b0bdbf6e2038b78d0e15aad9eaeb
elrik428 <labroukos80@gmail.com>
2019-05-10 14:55:48 +0300

    DutyFree_Reports

--- Reports/DutyFree/DutyFree  Annual Sums by Cardtype.sql
+++ Reports/DutyFree/DutyFree  Annual Sums by Cardtype.sql
@@ -1,3 +1,64 @@
+-- IMP_TRANSACT_D_2018
+--INSERT INTO [abc096].[IMP_TRANSACT_D_DF]
+--SELECT * from abc096.IMP_TRANSACT_D_2018
+--where mid = '000000120002800'
+
+select q.card_Type,
+SUM(CASE q.PROCCODE
+WHEN '200000' THEN -q.AMOUNT
+WHEN '020000' THEN -q.AMOUNT
+ELSE q.AMOUNT
+END)  as 'Total Amount'  ,
+--sum(q.TAMOUNT) as 'Total Amount' ,
+count(*) as 'Transaction Number'
+from
+(select AMOUNT, CASE SUBSTRING(MASK,1,2)
+WHEN '40' THEN 'VISA'
+WHEN '41' THEN 'VISA'
+WHEN '42' THEN 'VISA'
+WHEN '43' THEN 'VISA'
+WHEN '44' THEN 'VISA'
+WHEN '45' THEN 'VISA'
+WHEN '46' THEN 'VISA'
+WHEN '47' THEN 'VISA'
+WHEN '48' THEN 'VISA'
+WHEN '49' THEN 'VISA'
+WHEN '50' THEN 'MasterCard'
+WHEN '51' THEN 'MasterCard'
+WHEN '52' THEN 'MasterCard'
+WHEN '53' THEN 'MasterCard'
+WHEN '54' THEN 'MasterCard'
+WHEN '55' THEN 'MasterCard'
+WHEN '56' THEN 'MasterCard'
+WHEN '57' THEN 'MasterCard'
+WHEN '58' THEN 'MasterCard'
+WHEN '59' THEN 'MasterCard'
+WHEN '60' THEN 'Maestro'
+WHEN '61' THEN 'Maestro'
+WHEN '62' THEN 'UNIONPAY'--20151202
+WHEN '63' THEN 'Maestro'
+WHEN '64' THEN 'Maestro'
+WHEN '65' THEN 'Maestro'
+WHEN '66' THEN 'Maestro'
+WHEN '67' THEN 'Maestro'
+WHEN '68' THEN 'Maestro'
+WHEN '69' THEN 'Maestro'
+WHEN '34' THEN 'AMEX'
+WHEN '37' THEN 'AMEX'
+WHEN '30' THEN 'Diners'
+WHEN '36' THEN 'Diners'
+WHEN '38' THEN 'Diners'
+ELSE 'UNKNOWN TYPE'
+END as card_Type,
+PROCCODE
+ from abc096.IMP_TRANSACT_D_DF
+where mid = '000000120002800' and   MSGID in('0200','0220')
+   and RESPKIND = 'OK' and   REVERSED not in('F', 'A')
+ )q
+group by q.card_Type
+
+
+
 -- IMP_TRANSACT_D_2017
 select q.card_Type,sum(q.amount),count(*)
 from
