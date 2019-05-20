4b1b8678889bdafc3575e4c1b61306eb721ab58a
elrik428 <labroukos80@gmail.com>
2019-05-20 15:26:08 +0300

    iTunesDB_report_SmallChange

--- Reports/iTunes/iTunes report for specific customer.sql
+++ Reports/iTunes/iTunes report for specific customer.sql
@@ -5,3 +5,13 @@ SELECT [Customer],[TerminalID],[Storeno],[TrxDate],[TrxTime],[iTunes].[dbo].[TRA
  --where customer like '%LIDL%' AND [Category]='Apple'
  --where customer like '%LIDL%' AND [Category]='TopUp'
 ORDER BY [Customer],[TrxDate],[TrxTime]
+
+SELECT [Customer],[TerminalID],[Storeno],[TrxDate],[TrxTime],[iTunes].[dbo].[TRANSACTIONS_ALL].[EAN],
+ AAmount=CASE [Type]  WHEN 'D' THEN '-'+[Amount] WHEN 'A' THEN [Amount] END,[Receipt],[Trace],[Serial],TType = CASE [Type]  WHEN 'D' THEN 'Deactivation' WHEN 'A' THEN 'Activation' END,[User Name],[Name]
+FROM [iTunes].[dbo].[TRANSACTIONS_ALL] LEFT JOIN [iTunes].[dbo].[PRODUCTS] ON ([iTunes].[dbo].[PRODUCTS].EAN=[iTunes].[dbo].[TRANSACTIONS_ALL].EAN)
+WHERE
+  customer like '%CY%' AND [Category]='Xbox'
+ --customer like '%LIDL%' AND [Category]='Apple'
+ --customer like '%LIDL%' AND [Category]='TopUp'
+ and trxdate > '2019-05-05' and trxdate < '2019-05-13'
+ORDER BY [Customer],[TrxDate],[TrxTime]
