8b06ad720e987166812fe7309d5676af9104b256
elrik428 <labroukos80@gmail.com>
2019-09-30 15:53:40 +0300

    Updated sql script for New BIN to Transact

--- New BIN list_ Insert Procedure/Export file for Transact.sql
+++ New BIN list_ Insert Procedure/Export file for Transact.sql
@@ -1,3 +1,9 @@
-select a.BANKID, b.BANK,a.BIN,a.BINU,a.Product ,a.Brand from abc096.Products a
+select a.BANKID, b.BANK,a.BIN,a.BINU,
+case 
+when a.BANKID <> '0' then b.bank +' - ' +   a.Product 
+else a.Product
+end
+,a.Brand from abc096.Products a
 left join abc096.Banks b on a.BANKID = b.ID
+--where a.BANKID = '0'
 order by len(a.BIN)
